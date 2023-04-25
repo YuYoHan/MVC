484afbec364d9675737f78f76597181e1750c923
YuYoHan <zxzz8014@naver.com>
2023-04-25 17:00:44 +0900

    [Study] RequsetBodyJsonServlet

---
+++ src/main/java/com/example/servlet/basic/HelloData.java
@@ -0,0 +1,15 @@
+package com.example.servlet.basic;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class HelloData {
+    private String userName;
+    private int age;
+
+
+}
