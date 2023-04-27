ded8f54df5c91026462f945d480818f0fea5b589
YuYoHan <zxzz8014@naver.com>
2023-04-27 17:52:45 +0900

    [Study] 김영한MVC1

---
+++ src/main/java/com/example/servlet/basic/domain/member/Member.java
@@ -0,0 +1,24 @@
+package com.example.servlet.basic.domain.member;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Member {
+    private Long id;
+    private  String userName;
+    private  int age;
+
+    public Member() {
+    }
+
+    public Member(Long id, String userName, int age) {
+        this.id = id;
+        this.userName = userName;
+        this.age = age;
+    }
+
+    public Member(String hello, int i) {
+    }
+}
